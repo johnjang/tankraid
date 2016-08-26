b366db1154ed107e30a72cae4d011d1be646297f
John_Jang <jjohnjjang@gmail.com>
2016-08-25 20:34:12 -0700

    changed a lot of rendering stuff. cleaned up a lot

--- resources/js/main.js
+++ resources/js/main.js
@@ -8,17 +8,21 @@ document.body.appendChild(canvas);
 
 var imageObjects = {};
 var imgAddress = [];
-var terrain;
 
+var enemySprites = [];
+var strucSprites = [];
+var staticSprites = [];
 
 
 function loadResources() {
     //loading all the images
+    console.log("calling loadResources");
     imgAddress = [
         "resources/images/sprites.png",
         "resources/images/backgroundtile.bmp",
         "resources/images/grid.bmp",
-        "resources/images/mainTank.bmp"
+        "resources/images/mainTank.bmp",
+        "resources/images/enemyTank.bmp"
     ];
     loadImages(imgAddress, loadGame, imageObjects);
 
@@ -26,40 +30,49 @@ function loadResources() {
 
 }
 
-var dummies = [];
 function loadGame() {
-    var mainTank = imageObjects["resources/images/mainTank.bmp"];
-    dummies.push(new Sprite(mainTank, [400, 200], 80, 41, 'N'));
+    console.log("loadGame called");
+    var enemyTank = imageObjects["resources/images/enemyTank.bmp"];
+    enemySprites.push(new Sprite(enemyTank, [400, 200], 80, 41, 'N'));
+    enemySprites.push(new Sprite(enemyTank, [0, 0], 10, 41, 'S'));
+    enemySprites.push(new Sprite(enemyTank, [400, 200], 30, 41, 'E'));
+    enemySprites.push(new Sprite(enemyTank, [400, 200], 20, 41, 'W'));
     loop();
+}
+
+function renderBackground() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);   //clear the background
+    //redraw the static backgroun
+    //Maybe for next update, just paste the backgrounds permanently by creating
+    // another canvas? 
+    var backGround = imageObjects["resources/images/backgroundtile.bmp"];
+    var menuPanel = imageObjects["resources/images/grid.bmp"];
+    var mainTank = imageObjects["resources/images/mainTank.bmp"];
+    
+    ctx.fillStyle = ctx.createPattern(backGround, 'repeat');
+    ctx.fillRect(0, 0, canvas.width, canvas.height-50);
+
+    ctx.fillStyle = ctx.createPattern(menuPanel, 'repeat');
+    ctx.fillRect(0, 0+canvas.height-120, canvas.width, canvas.height-50);
+
+    ctx.drawImage(mainTank, 0,0,41,41,20,300,42,42);
 }
 
 
 function update(dtr) {
     console.log("updating");
-        var backGround = imageObjects["resources/images/backgroundtile.bmp"];
-        var backGround2 = imageObjects["resources/images/grid.bmp"];
-        var mainTank = imageObjects["resources/images/mainTank.bmp"];
-        terrain = ctx.createPattern(backGround, 'repeat');
-        ctx.fillStyle = terrain;
-        ctx.fillRect(0, 0, canvas.width, canvas.height-50);
-
-        ctx.fillStyle = ctx.createPattern(backGround2, 'repeat');
-        ctx.fillRect(0, 0+canvas.height-120, canvas.width, canvas.height-50);
-
-        ctx.drawImage(mainTank, 0,0,41,41,20,300,42,42);
-
-
-    dummies[0].updateLocation(dtr);
-    dummies[0].render(ctx);
-}
-
-function startGame() {
-    console.log("starting game");
-
+    renderBackground();
     
+    //updating enemy sprites
+    for(var i=0; i<enemySprites.length; i++) {
+        enemySprites[i].updateLocation(dtr);
+        enemySprites[i].render(ctx);
+    }
+
+
+
 }
 
-//the main function that starts everything
 function main() {
     loadResources();
 }
@@ -84,10 +97,5 @@ main();
 
 
 
-//context.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh)
-//sx, sy is top left corner of ORIGINAL FILE
-//sw, sh is width and height of original file
-//dx, dy is top left corner of DESTINATION CANVAS
-//dw, dh is width and height of picture in DESTINATION CANVAS
 
 
